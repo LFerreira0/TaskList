90d74109d01923fef47900ea2940adaa951fffcd
Lucas Ferreira <lucascandido.ferreira@hotmail.com>
2024-02-29 17:26:54 -0300

    Criação das models, controllers middlewares e autenticação

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '3ed2516bfa8f3d985f06e3f35a30ddf4',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'kmzwa8awaa',
+  database: 'tasklist',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
